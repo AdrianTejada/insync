b0493c65a54d1bc3aba3fbe71994b32fe209f27b
AdrianTejada <atejada@my.bcit.ca>
2022-09-06 14:31:49 -0700

    Text field styled and working

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Title from './Components/Title';
 import Functions from './Components/Functions';
 import AppContent from './Components/AppContent';
 import Modal from './Components/Modal';
+import TextField from './Components/TextField';
 
 function App() {
   return (
     <div>
       <Modal title='New Todo'>
-      
+        <TextField>
+          title*
+        </TextField>
       </Modal>
       <div className='app-base-light'>
         <div className='layout'>
