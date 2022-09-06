a61915830ba432f3ec73b3b7928113140d48919e
AdrianTejada <atejada@my.bcit.ca>
2022-09-06 13:33:26 -0700

    Created modal container

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import './Styles/index.css'
 import Title from './Components/Title';
 import Functions from './Components/Functions';
 import AppContent from './Components/AppContent';
+import Modal from './Components/Modal';
 
 function App() {
   return (
-    <div className='app-base-light'>
-      <div className='layout'>
-        <Title>Todo List</Title>
-        <div>
-          <Functions/>
-          <AppContent/>
+    <div>
+      <Modal title='New Todo'>
+      
+      </Modal>
+      <div className='app-base-light'>
+        <div className='layout'>
+          <Title>Todo List</Title>
+          <div>
+            <Functions/>
+            <AppContent/>
+          </div>
         </div>
-      </div>
+      </div>  
     </div>
   );
 }
