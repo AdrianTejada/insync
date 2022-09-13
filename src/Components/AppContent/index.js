468d187c4a156c84d5510b97f745a6d956c16ad1
AdrianTejada <atejada@my.bcit.ca>
2022-09-13 16:27:46 -0700

    Todo filter is working

--- src/Components/AppContent/index.js
+++ src/Components/AppContent/index.js
@@ -6,13 +6,22 @@ import { useSelector } from 'react-redux'
 export default function AppContent() {
   const todoList = useSelector((state) => state.todo.todoList);
   const theme = useSelector((state)=>state.theme.todoTheme);
+  const filter = useSelector((state)=>state.todo.todoFilter)
   const sortedTodoList = [...todoList];
 
   sortedTodoList.sort((a, b)=> new Date(a.time) - new Date(b.time));
+
+  const filteredTodoList = sortedTodoList.filter((item)=>{
+    if (filter === 'all') {
+      return true;
+    } else {
+      return item.status === filter;
+    }
+  })
   return (
     <div className={`app-content-${theme}`}>
       {sortedTodoList && sortedTodoList.length > 0
-      ? sortedTodoList.map((item)=><TodoItem key={item.id} item={item}/>)
+      ? filteredTodoList.map((item)=><TodoItem key={item.id} item={item}/>)
       : <div>no todos!</div>
       }
     </div>
