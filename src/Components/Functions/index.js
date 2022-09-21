850842a2b75bd3bab0b5ea6e6b04ff21a743a578
AdrianTejada <atejada@my.bcit.ca>
2022-09-20 17:09:55 -0700

    Replace about and add todo with icons

--- src/Components/Functions/index.js
+++ src/Components/Functions/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import Button from '../Button';
 import SelectButton from '../SelectButton';
 import './functions.css';
-import { MdPlaylistAdd } from "react-icons/md";
+import { RiPlayListAddLine } from "react-icons/ri";
 import Switch from '../Switch';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateFilter } from '../../Slices/todoSlice';
+import { motion } from 'framer-motion';
+import { AiOutlineInfo } from 'react-icons/ai';
+
+const tap = {scale: 0.9};
 
 export default function Functions({
   onTodoClick=()=>{},
   onAboutClick=()=>{},
 }) {
   const filter = useSelector((state)=>state.todo.todoFilter);
+  const todoList = useSelector((state) => state.todo.todoList);
+  const theme = useSelector((state)=>state.todo.todoTheme);
   const dispatch = useDispatch();
 
   const handleFilterChange = (event) => {
@@ -22,9 +27,9 @@ export default function Functions({
   return (
     <div className='functions'>
        <div>
-        <Button onClick={onTodoClick}>
-          new task<MdPlaylistAdd/>
-          </Button>
+        <motion.button whileTap={tap} onClick={onTodoClick} className={`add-todo-${theme}`}>
+          <RiPlayListAddLine/>
+        </motion.button>
         <Switch/>
        </div>
        <div>
@@ -32,10 +37,10 @@ export default function Functions({
             <option value='all'>all tasks</option>
             <option value='incomplete'>incomplete</option>
             <option value='complete'>complete</option>
-          </SelectButton>
-          <Button variant={'secondary'} onClick={onAboutClick}>
-            about
-          </Button>
+          </SelectButton> 
+          <motion.button whileTap={tap} onClick={onAboutClick} className={`info-${theme}`}>
+            <AiOutlineInfo/>
+          </motion.button>
        </div>
     </div>
   )
